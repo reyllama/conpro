64d1633d94fdf507f30b6ec881e53730e440d567
reyllama <rin4616@gmail.com>
2022-04-25 21:01:09 +0900

    sample data

---
+++ sample_data.py
@@ -0,0 +1,22 @@
+import shutil, os
+from os import path
+import random
+
+tasks = [f"task_{idx}" for idx in range(1, 8)]
+
+source_path = "/workspace/conpro_experiments/data/AnimalFace/cl"
+target_path = "/workspace/conpro_experiments/data/AnimalFace/cl-10"
+
+n_shots = 10
+
+for task in tasks:
+    task_dir = path.join(source_path, task)
+    sub_dir = os.listdir(task_dir)[0]
+    target_dir = path.join(task_dir, sub_dir)
+    files = os.listdir(target_dir)
+    chosen_files = random.sample(files, k=n_shots)
+    for f in chosen_files:
+        save_dir = path.join(target_path, task, sub_dir)
+        if not path.isdir(save_dir):
+            os.makedirs(save_dir)
+        shutil.copyfile(path.join(target_dir, f), path.join(save_dir, f))
